ff5b7bc43494c5c943828823ad42bff5584ac9d3
Maad maaD <erics.erics.000@gmail.com>
2018-11-02 17:51:32 +0100

    new

--- script.py
+++ script.py
@@ -2,7 +2,7 @@ def fibo(n):
     if n in [0,1]:
         return n
     else:
-        return fibo(n-1) + fibo(n-2)
+        return fibo(n-1) + fibo(n-2) + 1
 
 num = int(input("Give me a number: "))
 print("fibo({}) = {}".format(num, fibo(num)))
