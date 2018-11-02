c511ad942c1aae0d57b6fa90a785a06c9d36ccdc
Maad maaD <erics.erics.000@gmail.com>
2018-11-02 17:52:50 +0100

    new feature 15

--- script.py
+++ script.py
@@ -1,8 +1,10 @@
 def fibo(n):
     if n in [0,1]:
         return n
+    elif n == 15:
+        return 999
     else:
-        return fibo(n-1) + fibo(n-2) + 1
+        return fibo(n-1) + fibo(n-2)
 
 num = int(input("Give me a number: "))
 print("fibo({}) = {}".format(num, fibo(num)))
